09f8323078b82c56720be3805593b39cc447aa64
si2 <si2@si2_02.ESG.IPSantarem.local>
2019-05-14 11:49:06 +0100

    implementada a clasee data

--- src/es/datautil/Data.java
+++ src/es/datautil/Data.java
@@ -11,15 +11,13 @@ public class Data {
 	 */
 	public static boolean anoBissexto(int ano)
 	{
-		// IMPLEMENTAR
-		// Implemente a lógica a partir do Fluxograma dado em anexo!
-		
-		
-		
-		
-		
-		
-		return true;
+		if (ano%4 ==0) {
+			if (ano%100==0) {
+				if (ano%400==0) {
+				}else {return true;}
+			}else {return false;}
+		}else {return true;}
+	return false;
 	}
 	
 	
@@ -52,6 +50,19 @@ public class Data {
 	public static int diasMes( Meses mes, int ano)
 	{
 		// Testa se o ano é bisseexto caso o mes seja Meses.FEVEREIRO
+		
+		if (mes.getNumeroMes()==2) {
+		if(mes.getNumeroMes()==2 && anoBissexto(ano) == true) {
+			return 29;
+		}if(mes.getNumeroMes()==2 && anoBissexto(ano) == false) {
+			return 28;}
+		else if( (mes.getNumeroMes()<=7 && mes.getNumeroMes() %2!=0)||(mes.getNumeroMes()>=8 && mes.getNumeroMes() %2==0)){
+			return 30;
+			
+		}
+	
+	}
+		
 		
 			
 		
